e4ebb491571c840860176cdf0c4dccd5e1315808
Milton Yano <miltonyano@gmail.com>
2020-07-15 16:40:44 -0400

    Migrations

--- src/modules/customers/infra/typeorm/entities/Customer.ts
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -6,15 +6,21 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('customers')
 class Customer {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
+  @Column()
   created_at: Date;
 
+  @Column()
   updated_at: Date;
 }
 
